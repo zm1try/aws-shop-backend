4d66ed5aa7c1a715233be818b160f4091af7d418
Dzmitry_Yarash <dzmitry_yarash@epam.com>
2023-05-03 22:10:57 +0300

    task-7: added response params settings

--- authorization-service/src/functions/basicAuthorizer/index.ts
+++ authorization-service/src/functions/basicAuthorizer/index.ts
@@ -2,4 +2,12 @@ import { handlerPath } from '@libs/handler-resolver';
 
 export default {
   handler: `${handlerPath(__dirname)}/handler.main`,
+  events: [
+    {
+      http: {
+        method: 'get',
+        path: 'auth',
+      }
+    },
+  ],
 };
